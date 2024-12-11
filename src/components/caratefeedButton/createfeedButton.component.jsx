de15154413f69796ee76899207eab68ccfa999df
sudarshan <sudarshan@kringle.ai>
2024-12-11 08:14:21 +0530

    create feed button added

---
+++ src/components/caratefeedButton/createfeedButton.component.jsx
@@ -0,0 +1,12 @@
+import "./createfeedButton.style.scss"
+import FeedButton from "../../assets/create-feed.png"
+
+const CreateFeedButton = () => {
+    return (
+        <div>
+            <img src={FeedButton} alt="" />
+        </div>
+    )
+};
+
+export default CreateFeedButton;
--- src/routes/feeds/feeds.component.jsx
+++ src/routes/feeds/feeds.component.jsx
@@ -1,10 +1,15 @@
 import "./feeds.style.scss";
 
 import WelcomeUser from "../../components/user/welcomeuser/welcomeuser.component";
+import CreateFeedButton from "../../components/caratefeedButton/createfeedButton.component";
 const Feeds = () => {
     return (
-        <div>
+        <div className="user-feed-container">
             {<WelcomeUser />}
+            <h2>Feeds</h2>
+            <div className="feed-create-button">
+                <CreateFeedButton />
+           </div>
         </div>
     )
 }
